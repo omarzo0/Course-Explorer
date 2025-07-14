6647e3b976452aca7b26f2fbadfd4e13b69a4bd7
omar_khaled_72 <omarkhaled202080@gmail.com>
2025-07-14 15:55:31 +0300

    first Publish
    install independence libraries

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <section className='bg-dark'>
+          <div className='flex flex-col items-center justify-center min-h-screen text-white layout'>
+            <h1>Test</h1>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
